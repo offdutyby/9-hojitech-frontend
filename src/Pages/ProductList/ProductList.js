432c080bd7627e344da0557a8d017422b1cad8eb
HyunJungho <offdutybyblo@gmail.com>
2020-06-23 11:53:52 +0900

    상품리스트 페이지에  header부분 UI구현했습니다. (img,text 추가)

--- src/Pages/ProductList/ProductList.js
+++ src/Pages/ProductList/ProductList.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react'
+import '../ProductList/productlist.scss'
+
+// <img alt="" src=""/>
 
 class ProductList extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-                 
-        }
-    }
-
     render() {
         return (
             <div>
-                
+                <div className="header">
+                    <div className="navContainer"> 
+                        <div className="navTextContainer">
+                            <div className="mouseProductName">
+                                <a href="/">마우스+키보드</a>/마우스
+                            </div>
+                            <div className="mouseText">
+                            마우스
+                            </div>
+                            <div className="logitechMouse">
+                            로지텍 마우스
+                            </div>
+                        </div>
+                        <img className="navMouseImg" src="https://www.logitech.com/assets/64464/mice.png" alt=""/>
+                    </div>
+                </div>
+                <div className="mainArticle">
+                <div className="filterHideContainer">
+                    <div className="filterHide">
+                    <img className="filterIcon" src="https://www.logitech.com/images/icons/filter-toggle.svg" alte=""/>
+                        필터 숨기기
+                    </div>  
+                </div>
+                <div className="cardList">
+                    
+                </div>  
+                </div>
             </div>
         )
     }
 }
-
 export default ProductList
