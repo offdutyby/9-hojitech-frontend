ab98d33eb2eca144d2aa7bf7a767a4c27cb15551
HyunJungho <offdutybyblo@gmail.com>
2020-06-23 21:22:30 +0900

    package.json에 node-sass 추가설치했습니다.

--- src/Pages/ProductList/ProductList.js
+++ src/Pages/ProductList/ProductList.js
@@ -4,6 +4,25 @@ import '../ProductList/productlist.scss'
 // <img alt="" src=""/>
 
 class ProductList extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            visible:"collectionList"
+        }        
+    }
+
+    clickHandler=(e)=>{
+        if(this.state.visible==="collectionList"){
+            this.setState({
+                visible:"collectionListActive"
+            })
+        }else{
+            this.setState({
+                visible:"collectionList"
+            })
+        }        
+    }
+
     render() {
         return (
             <div>
@@ -11,7 +30,7 @@ class ProductList extends Component {
                     <div className="navContainer"> 
                         <div className="navTextContainer">
                             <div className="mouseProductName">
-                                <a href="/">마우스+키보드</a>/마우스
+                                <a className="mouseKeyboardLink" href="/">마우스+키보드</a>/마우스
                             </div>
                             <div className="mouseText">
                             마우스
@@ -24,15 +43,44 @@ class ProductList extends Component {
                     </div>
                 </div>
                 <div className="mainArticle">
-                <div className="filterHideContainer">
-                    <div className="filterHide">
-                    <img className="filterIcon" src="https://www.logitech.com/images/icons/filter-toggle.svg" alte=""/>
-                        필터 숨기기
+                    <div className="productListMainContainer">
+                        <div className="filterHideContainer">
+                        <div className="filterHide">
+                            <img className="filterIcon"  src="https://www.logitech.com/images/icons/filter-toggle.svg" alt=""/>
+                            <span>필터 숨기기</span>
+                        </div>  
+                        </div>
+                        <div className="filterSidebar">
+                            <div className="collectionContainer">
+                                <div className="collection" onClick={this.clickHandler}>
+                                    <span className="collectionName">컬렉션</span>
+                                    <img className="minusImg"  alt="" src="https://www.logitech.com/images/icons/icon-collapse.svg" />
+                                </div>
+                                <div className={this.state.visible}>
+                                    <div >
+                                    MX 퍼포먼스
+                                    </div>
+                                    <div >
+                                    인체공학 제품군
+                                    </div>
+                                    <div >
+                                    멀티 디바이스
+                                    </div>
+                                    <div >
+                                    무선
+                                    </div>
+                                    <div >
+                                    비즈니스용
+                                    </div>
+                                    <div >
+                                    게이밍 마우스
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="Content">
+                        </div>
                     </div>  
-                </div>
-                <div className="cardList">
-                    
-                </div>  
                 </div>
             </div>
         )
