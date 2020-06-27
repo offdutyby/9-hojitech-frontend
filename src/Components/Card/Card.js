545626971f20ef4198bc9556c55b1e002f2bbd0c
HyunJungho <offdutybyblo@gmail.com>
2020-06-27 11:31:07 +0900

    Card.js,ProductList.js  코드 리팩토링, 로직 간소화 했습니다.

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,47 +2,49 @@ import React, { Component } from 'react'
 import './card.scss'
 
 class Card extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            cardHoverColor:{backgroundColor:'#f6f6f6'},
-            poroductNameHide:{display:'none'},
-            isMouseIn: true
-        }
+  constructor(props){
+    super(props);
+    this.state={
+      cardHoverColor:{backgroundColor:'#f6f6f6'},
+      poroductNameHide:{display:'none'},
+      isMouseIn: true
     }
+  }
 
-    mouseOverHandler= () => {
-        const { isMouseIn } = this.state
+  mouseOverHandler= () => {
+      const { isMouseIn } = this.state
 
-        this.setState({
-            cardHoverColor:  isMouseIn ? { backgroundColor: [this.props.hoverColor] } : { backgroundColor: "#f6f6f6" },
-            poroductNameHide: isMouseIn ? { display: "block" } : { display: "none" },
-            isMouseIn: !isMouseIn,
-        })
-    }
+      this.setState({
+        cardHoverColor: { backgroundColor: isMouseIn ? [this.props.hoverColor] : "#f6f6f6" },
+        poroductNameHide: { display: isMouseIn ?  "block" :  "none" },
+        isMouseIn: !isMouseIn,
+      })
+  }
 
 
-    render() {
-        return (
-            <div className="Card" >
-                <div className="cardContainer"  onMouseEnter={this.mouseOverHandler} onMouseLeave={this.mouseOverHandler}>
-                    <div className="productCard" style={this.state.cardHoverColor}>
-                        <img className="productCardImg" alt="" src={this.props.imgSrc} />
-                        <div className="cardPick"style={this.state.poroductNameHide}>
-                        <input className="check" type="checkbox" />선택한 항목을 비교
-                        </div>
-                    </div>
-                    <div className="cardProductName">
-                        {this.props.name}
-                    </div>
-                    <div className="cardProductInfo" style={this.state.poroductNameHide}>
-                        {this.props.thumbnailDescription}
-                    </div>
-                    
-                </div>                
-            </div>
-        )
-    }
+  render() {
+    const {cardHoverColor, poroductNameHide} = this.state
+    const {imgSrc, name, thumbnailDescription} = this.props
+
+    return (
+      <div className="Card" >
+        <div className="cardContainer"  onMouseEnter={this.mouseOverHandler} onMouseLeave={this.mouseOverHandler}>
+          <div className="productCard" style={cardHoverColor}>
+            <img className="productCardImg" alt="" src={imgSrc} />
+              <div className="cardPick"style={poroductNameHide}>
+              <input className="check" type="checkbox" />선택한 항목을 비교
+              </div>
+          </div>
+          <div className="cardProductName">
+            {name}
+          </div>
+          <div className="cardProductInfo" style={poroductNameHide}>
+            {thumbnailDescription}
+          </div>
+        </div>                
+      </div>
+    )
+  }
 }
 
 export default Card
