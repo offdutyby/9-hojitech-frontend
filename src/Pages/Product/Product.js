b0fa3cf1abac94a833eedebd304dcc1a7877daf5
HyunJungho <offdutybyblo@gmail.com>
2020-06-28 19:16:53 +0900

    product detail 페이지 UI구현, 기본적인 헤더 폼 구현

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -31,23 +31,35 @@ class Product extends Component {
                       그라파이트 토날
                     </p>
                     <div className="colorImgContainer">
-                    <img className="colorImg" src="https://assets.logitech.com/assets/65123/24/wireless-mouse-m590-multi-device-silent.jpg" alt=""></img>
-                    <img className="colorImg" src="https://assets.logitech.com/assets/65123/24/wireless-mouse-m590-multi-device-silent.jpg" alt=""></img>
-                    <img className="colorImg" src="https://assets.logitech.com/assets/65123/24/wireless-mouse-m590-multi-device-silent.jpg" alt=""></img>
+                      <img className="colorImg" src="https://assets.logitech.com/assets/65123/24/wireless-mouse-m590-multi-device-silent.jpg" alt=""></img>
+                      <img className="colorImg" src="https://assets.logitech.com/assets/65123/25/wireless-mouse-m590-multi-device-silent.jpg" alt=""></img>
+                      <img className="colorImg" src="https://assets.logitech.com/assets/65123/26/wireless-mouse-m590-multi-device-silent.jpg" alt=""></img>
                     </div>
                   </div>
                   <div className="specPrice">
-                    <img className="plusImg" src="https://lh3.googleusercontent.com/proxy/LN7Af6u9o6ZlQC88GR-kg6PJpdYdihv4ytpaU09QnlD1TZj5CI1afoG9Nitx7IHHWo5R89JLrkvvi0xkOwAx" />
-                    <span>사양</span>
+                    <img className="plusImg" src="https://icons.iconarchive.com/icons/icons8/ios7/256/User-Interface-Plus-icon.png" />
+                    <span className="spec">사양</span>
+                    <span className="price">₩ 10.000</span>
+                  </div>
+                  <div className="quantityContainer">
+                    <input className="quantity"  type="number" min="1" max="100" />
                   </div>
                   <div className="btnContainer">
                     <div className="buyBtn">
+                      구매하기
                     </div>
                     <div className="cartBtn">
+                      <img className="cartImg" src="https://www.brandeps.com/icon-download/C/Cart-icon-vector-01.svg" alt="" />
                     </div>
                   </div>
                 </div>
               </div>
+              <ul className="imgCollection">
+                <li><img className="img" src="https://assets.logitech.com/assets/65123/wireless-mouse-m590-multi-device-silent.png" alt="" /></li>
+                <li><img className="img" src="" /></li>
+                <li><img className="img" src="" /></li>
+                <li><img className="img" src="" /></li>
+              </ul>
             </div>
           </header>
           <article>
