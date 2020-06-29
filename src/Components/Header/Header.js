1cb62f0f4869573daa7076da4093c6e264f1e3be
이한별 <maleebook@MaLeeBook.local>
2020-06-29 13:08:28 +0900

    헤더에 장바구니 추가, 헤더 Link 연결

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import HeaderSearch from "./HeaderSearch";
+import { Link } from "react-router-dom";
 import LogoBk from "../../Images/logo_bk.png";
 import "./header.scss";
 
@@ -8,45 +9,45 @@ class Header extends Component {
     super(props);
     this.state = {
       activeTab: null,
-      headerSearch:false,
-      headerSearchPageInput:"",
-      headerSearchPageImage:"",
-      
+      headerSearch: false,
+      headerSearchPageInput: "",
+      headerSearchPageImage: "",
     };
   }
 
-  seachInputClick=()=>{
+  seachInputClick = () => {
     this.setState({
-      headerSearch:true
-    })
-  }
-  
-  inputChange=(e)=>{
-    this.setState({
-      headerSearchPageInput : e.target.value,
-    })
-    if(this.state.headerSearchPageInput.length!==0){
-      this.setState({
-      headerSearchPageImage:"https://img.icons8.com/material-rounded/24/000000/multiply--v1.png",
-      })
-    }else{
-      this.setState({
-        headerSearchPageImage:"",
-        })
-    }
-  }
-  clicks=()=>{
-    this.setState({
-      headerSearch:false,
-      headerSearchPageInput:'',
-    })
-  }
+      headerSearch: true,
+    });
+  };
 
-  hoverHandler = id => {
+  inputChange = (e) => {
+    this.setState({
+      headerSearchPageInput: e.target.value,
+    });
+    if (this.state.headerSearchPageInput.length !== 0) {
+      this.setState({
+        headerSearchPageImage:
+          "https://img.icons8.com/material-rounded/24/000000/multiply--v1.png",
+      });
+    } else {
+      this.setState({
+        headerSearchPageImage: "",
+      });
+    }
+  };
+  clicks = () => {
+    this.setState({
+      headerSearch: false,
+      headerSearchPageInput: "",
+    });
+  };
+
+  hoverHandler = (id) => {
     if (this.state.activeTab === id) this.setState({ activeTab: null });
-    else this.setState({ activeTab: id })
-  }
-    
+    else this.setState({ activeTab: id });
+  };
+
   render() {
     return (
       <div className="Header">
@@ -54,19 +55,19 @@ class Header extends Component {
           <div className="headerTopWrap">
             <div className="haederTop">
               <div className="headerTopLeft">
-                <a href="/#">
+                <Link to="/main">
                   <img
                     src="https://www.logitech.com/content/dam/logitech/common/header/logitechg-bottom.svg"
                     alt="logitechG"
                   ></img>
-                </a>
-                <a href="/#">
+                </Link>
+                <a>
                   <img
                     src="https://www.logitech.com/content/dam/logitech/common/header/jaybird-overlay.svg"
                     alt="jaybirdsoprt"
                   ></img>
                 </a>
-                <a href="/#">
+                <a>
                   <img
                     src="https://www.logitech.com/content/dam/logitech/common/header/ue-bottom.svg"
                     alt="ultimateears"
@@ -74,107 +75,152 @@ class Header extends Component {
                 </a>
               </div>
               <div className="headerTopRight">
-                <a href="/#">
+                <a>
                   <img src="https://www.logitech.com/images/flags/south-korea.gif"></img>
                   KO
                 </a>
-                <a href="/#">내 계정</a>
+                <Link to="/cart">장바구니</Link>
+                <Link to="/login">내 계정</Link>
               </div>
             </div>
           </div>
           <div className="headerMain">
             <div className="headerMainRight">
-              <a href="/#">
+              <Link to="/main">
                 <img src={LogoBk} alt="mainlogo"></img>
-              </a>
+              </Link>
             </div>
             <nav>
-              <div className="mainNav" onMouseOver={() => this.hoverHandler(0)} onMouseOut={() => this.hoverHandler(0)}>
+              <div
+                className="mainNav"
+                onMouseOver={() => this.hoverHandler(0)}
+                onMouseOut={() => this.hoverHandler(0)}
+              >
                 제품
-                  <div className={`mainNavWrap ${this.state.activeTab === 0 ? "show" : "hide"}`}>
-                    <ul>
-                      <li>마우스+키보드</li>
-                      <li>마우스</li>
-                      <li>콤보</li>
-                      <li>키보드</li>
-                      <li>iPad 키보드</li>
-                      <li>태블릿 키보드</li>
-                    </ul>
-                    <ul>
-                      <li>프레젠테이션 리모컨</li>
-                      <li>Spotlight</li>
-                      <li>전체 프레젠테이션 리모컨</li>
-                    </ul>
-                    <ul>
-                      <li>모바일 장치</li>
-                      <li>iPad 키보드</li>
-                      <li>iPad 케이스</li>
-                      <li>태블릿 키보드</li>
-                      <li>스마트폰 액세서리</li>
-                    </ul>
-                  </div>
+                <div
+                  className={`mainNavWrap ${
+                    this.state.activeTab === 0 ? "show" : "hide"
+                  }`}
+                >
+                  <ul>
+                    <li>마우스+키보드</li>
+                    <li>마우스</li>
+                    <li>콤보</li>
+                    <li>키보드</li>
+                    <li>iPad 키보드</li>
+                    <li>태블릿 키보드</li>
+                  </ul>
+                  <ul>
+                    <li>프레젠테이션 리모컨</li>
+                    <li>Spotlight</li>
+                    <li>전체 프레젠테이션 리모컨</li>
+                  </ul>
+                  <ul>
+                    <li>모바일 장치</li>
+                    <li>iPad 키보드</li>
+                    <li>iPad 케이스</li>
+                    <li>태블릿 키보드</li>
+                    <li>스마트폰 액세서리</li>
+                  </ul>
+                </div>
               </div>
-                <div className="mainNav" onMouseOver={() => {this.hoverHandler(1)}} onMouseOut={() => {this.hoverHandler(1)}}>
-                  솔루션
-                <div className={`mainNavWrap ${this.state.activeTab === 1 ? "show" : "hide"}`}>
-                <ul>
-                  <li>프리미엄 컬렉션</li>
-                  <li>코딩용 MX</li>
-                  <li>창작용 MX</li>
-                  <li>Master 시리즈 제품</li>
-                </ul>
-                <ul>
-                  <li>집에서 근무하기</li>
-                  <li>홈 오피스의 필수 사항</li>
-                  <li>WFH 리소스</li>
-                </ul>
-                <ul>
-                  <li>교육</li>
-                  <li>집에서 학습하기</li>
-                </ul>
-              </div>
-              </div>
-              
-              <div className="mainNav" onMouseOver={() => {this.hoverHandler(2)}} onMouseOut={() => {this.hoverHandler(2)}}>
-                  비즈니스
-              <div className={`mainNavWrap ${this.state.activeTab === 2 ? "show" : "hide"}`}>
-                <ul>
-                  <li>비즈니스</li>
-                  <li>카메라</li>
-                  <li>웹캠</li>
-                  <li>헤드셋</li>
-                </ul>
-                <ul>
-                  <li>비디오 협업</li>
-                  <li>개요</li>
-                  <li>제품 보기</li>
-                  <li>리소스</li>
-                  <li>파트너</li>
-                  <li>구매 문의</li>
-                  <li>리셀러 찾기</li>
-                </ul>
-              </div>
+              <div
+                className="mainNav"
+                onMouseOver={() => {
+                  this.hoverHandler(1);
+                }}
+                onMouseOut={() => {
+                  this.hoverHandler(1);
+                }}
+              >
+                솔루션
+                <div
+                  className={`mainNavWrap ${
+                    this.state.activeTab === 1 ? "show" : "hide"
+                  }`}
+                >
+                  <ul>
+                    <li>프리미엄 컬렉션</li>
+                    <li>코딩용 MX</li>
+                    <li>창작용 MX</li>
+                    <li>Master 시리즈 제품</li>
+                  </ul>
+                  <ul>
+                    <li>집에서 근무하기</li>
+                    <li>홈 오피스의 필수 사항</li>
+                    <li>WFH 리소스</li>
+                  </ul>
+                  <ul>
+                    <li>교육</li>
+                    <li>집에서 학습하기</li>
+                  </ul>
+                </div>
               </div>
 
-                <div className="mainNav" onMouseOver={()=>{this.hoverHandler(3)}} onMouseOut={() => {this.hoverHandler(3)}}>
-                  지원
-              <div className={`mainNavWrap ${this.state.activeTab === 3 ? "show" : "hide"}`}>
-                <ul>
-                  <li>고객지원</li>
-                  <li>지원 홈</li>
-                  <li>제품 등록</li>
-                  <li>소프트웨어 다운로드</li>
-                </ul>
-                <ul>
-                  <li>비즈니스 지원</li>
-                  <li>협업 지원</li>
-                  <li>비디오 협업</li>
-                  <li>예비 부품 </li>
-                  <li>비디오 협업 다운로드</li>
-                  <li>연락처</li>
-                  <li>리셀러 찾기</li>
-                </ul>
+              <div
+                className="mainNav"
+                onMouseOver={() => {
+                  this.hoverHandler(2);
+                }}
+                onMouseOut={() => {
+                  this.hoverHandler(2);
+                }}
+              >
+                비즈니스
+                <div
+                  className={`mainNavWrap ${
+                    this.state.activeTab === 2 ? "show" : "hide"
+                  }`}
+                >
+                  <ul>
+                    <li>비즈니스</li>
+                    <li>카메라</li>
+                    <li>웹캠</li>
+                    <li>헤드셋</li>
+                  </ul>
+                  <ul>
+                    <li>비디오 협업</li>
+                    <li>개요</li>
+                    <li>제품 보기</li>
+                    <li>리소스</li>
+                    <li>파트너</li>
+                    <li>구매 문의</li>
+                    <li>리셀러 찾기</li>
+                  </ul>
+                </div>
               </div>
+
+              <div
+                className="mainNav"
+                onMouseOver={() => {
+                  this.hoverHandler(3);
+                }}
+                onMouseOut={() => {
+                  this.hoverHandler(3);
+                }}
+              >
+                지원
+                <div
+                  className={`mainNavWrap ${
+                    this.state.activeTab === 3 ? "show" : "hide"
+                  }`}
+                >
+                  <ul>
+                    <li>고객지원</li>
+                    <li>지원 홈</li>
+                    <li>제품 등록</li>
+                    <li>소프트웨어 다운로드</li>
+                  </ul>
+                  <ul>
+                    <li>비즈니스 지원</li>
+                    <li>협업 지원</li>
+                    <li>비디오 협업</li>
+                    <li>예비 부품 </li>
+                    <li>비디오 협업 다운로드</li>
+                    <li>연락처</li>
+                    <li>리셀러 찾기</li>
+                  </ul>
+                </div>
               </div>
             </nav>
             <div className="headerMainLeft">
@@ -186,9 +232,20 @@ class Header extends Component {
                 defaultValue="검색"
               ></input>
             </div>
-            <div className={`headerSearchPage ${this.state.headerSearch ? "show" : "hide"}`}>
-              <input placeholder="검색" onChange={this.inputChange} defaultValue={this.state.headerSearchPageInput}></input>
-              <img onClick={this.clicks} src={this.state.headerSearchPageImage}></img>
+            <div
+              className={`headerSearchPage ${
+                this.state.headerSearch ? "show" : "hide"
+              }`}
+            >
+              <input
+                placeholder="검색"
+                onChange={this.inputChange}
+                defaultValue={this.state.headerSearchPageInput}
+              ></input>
+              <img
+                onClick={this.clicks}
+                src={this.state.headerSearchPageImage}
+              ></img>
               <div className="headerSearchResult"></div>
             </div>
           </div>
--- src/Pages/Cart/CartProduct.js
+++ src/Pages/Cart/CartProduct.js
@@ -1,49 +1,38 @@
 import React, { Component } from "react";
 
-
-
 class CartProduct extends Component {
-  state ={
-    arr: [],
-    allprice:''
-  }
+  deleteIndex = () => {
+    this.props.deleteProds(this.props.id);
+  };
 
-
-    deleteIndex = () => {
-      this.props.deleteProd(this.props.id);
-    }
-
-    sumAllPrice = (() => this.props.totalPrice(this.props.productPrice))()
-
+  sumAllPrice = (() => this.props.totalPrice(this.props.productPrice))();
 
   render() {
     return (
-        <div className="CartProduct">
-          {/* <input type="checkbox" className="cartCheckbox"></input> */}
-          <img
-            className="thumbnail"
-            src={this.props.thumbnailImage}
-          ></img>
-          <div className="cartText">
-            <span className="productName">
-              {this.props.productName}
-              <img
-                className="productColor"
-                src="https://assets.logitech.com/assets/65123/26/wireless-mouse-m590-multi-device-silent.jpg"
-              />
-            </span>
-    <span className="productCount">{this.props.id}</span>
-    <span className="productPrice" /* onClick={this.sumAllPrice} */> {this.props.productPrice.toLocaleString()}</span>
-   
-
-          </div>
-          <img
-            className="productDelete"
-            onClick={this.deleteIndex}
-            src="https://img.icons8.com/material-two-tone/24/000000/multiply.png"
-          />
+      <div className="CartProduct">
+        <img className="thumbnail" src={this.props.thumbnailImage} alt="" />
+        <div className="cartText">
+          <span className="productName">
+            {this.props.productName}
+            <img
+              className="productColor"
+              src="https://assets.logitech.com/assets/65123/26/wireless-mouse-m590-multi-device-silent.jpg"
+              alt=""
+            />
+          </span>
+          <span className="productCount">{this.props.id}</span>
+          <span className="productPrice">
+            {" "}
+            {this.props.productPrice.toLocaleString()}
+          </span>
         </div>
-
+        <img
+          className="productDelete"
+          onClick={this.deleteIndex}
+          src="https://img.icons8.com/material-two-tone/24/000000/multiply.png"
+          alt=""
+        />
+      </div>
     );
   }
 }
